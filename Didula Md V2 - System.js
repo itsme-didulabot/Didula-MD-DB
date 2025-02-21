08eb7b36730fbc23fba3810888047a411adff432
itsme-didulabot <ddidula034@gmail.com>
2025-02-21 08:45:06 +0530

    Update Didula Md V2 - System.js

--- Didula Md V2 - System.js
+++ Didula Md V2 - System.js
@@ -1,1 +1,60 @@
-// Update Soon
+// Update සූන්
+
+
+const { cmd, commands } = require('../lib/command');
+const config = require('../settings');
+const si = require('systeminformation');
+const pdfUrl = "https://i.ibb.co/tC37Q7B/20241220-122443.jpg";
+const fs = require('fs');
+const path = require('path')
+
+
+
+cmd({
+    pattern: "repo",
+    desc: "repo the bot",
+    react: "📡",
+    category: "owner",
+    filename: __filename
+},
+async(conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+    try {
+        let dec = `*DIDULA MD V2 NEW UPDATE✅*
+
+*⭕ REPO URL*
+\`\`\`https://github.com/itsme-didulabot/Didula-MD-V2\`\`\`
+
+*⭕ GET SESSION ID*
+\`\`\`https://prabath-md-pair-web-v2-slk.koyeb.app/pair\`\`\`
+
+*⭕ HEROKU DEPLOY*
+\`\`\`https://dashboard.heroku.com/new-app?template=https://github.com/itsme-didulabot/Didula-MD-V2\`\`\`
+
+SPECIAL FEATURES 👀
+
+> Chanel working
+> Heart React
+> Anti Bug Message
+> Anti Bad/Bot/Link/Call
+> AI Chat
+> Auto Status Seen React and Reply
+> Anti Once View
+> Send Status to reply
+> Anti Delete
+> Commands 100+
+
+*AUTO PLUGIN UPDATE*
+
+📥FOLLOW FOR UPDATE
+https://whatsapp.com/channel/0029VaqqF4GDTkJwKruLSK2f`;
+
+        await conn.sendMessage(from, {
+            image: {url: 'https://i.ibb.co/tC37Q7B/20241220-122443.jpg'},
+            caption: dec
+        }, {quoted: mek});
+
+    } catch(e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
